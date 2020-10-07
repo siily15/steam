3aa67abf52e9c68329b6eea5b87a45d03dd8a63b
siily <arnokosk15@gmail.com>
2020-10-07 11:28:12 +0300

    style for prim nad sec nav

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    future: {
+        removeDeprecatedGapUtilities: true,
+        purgeLayersByDefault: true,
+    },
+    purge: [],
+    theme: {
+        extend: {
+            colors: {
+                'navy-500': "#171a21",
+                'green-steam': "#5c7e10",
+                'navy-400': "rgba( 62, 126, 167, 0.8)",
+                'whity': "rgba(16, 21, 25, 0.3)",
+                'line': "rgb(227, 234, 239) 5%,rgb(199, 213, 224) 95%",
+                'bord-colr': " rgba(16, 21, 25, 0.3)",
+            }
+        },
+    },
+    variants: {},
+    plugins: [],
+}
